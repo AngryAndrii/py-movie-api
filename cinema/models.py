a2944662d7e1b1cb7281f0412e742a07a3c139b7
Andrii Chychkan <mywork1728@gmail>
2025-12-24 15:37:04 +0200

    added views for get post put delete

--- cinema/models.py
+++ cinema/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=100, blank=True, default="")
     description = models.TextField(blank=True, default="")
     duration = models.PositiveIntegerField()
 
     class Meta:
-        ordering=["title"]
+        ordering = ["title"]
 
     def __str__(self):
-        return f"{self.name} duration: {self.duration}"
+        return f"{self.title} duration: {self.duration}"
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path
 
-from cinema.views import (movies_list,
-                          movie_create,
-                          movie_delete,
-                          movie_detail,
-                          movie_update)
-from core.urls import urlpatterns
+from cinema.views import movies_list, movie_detail
 
 app_name = "cinema"
 
 urlpatterns = [
     path("cinema/movies/", movies_list, name="movie_list"),
-    path("cinema/movies/<int:pk>", movie_detail, name="movie_detail"),
-    path("cinema/movies/", movie_create, name="movie_create"),
-    path("cinema/movies/<int:pk>", movie_update, name="movie_update"),
-    path("cinema/movies/<int:pk>", movie_delete, name="movie_delete")
+    path("cinema/movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
